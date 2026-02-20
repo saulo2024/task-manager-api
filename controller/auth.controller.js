99998457899d76508375db3618697a3c24761997
estevesdev <sauloestevesdealmeida27@gmail.com>
2026-02-20 09:44:52 -0400

    feat: sign-in logic and fix named export issues

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -31,3 +31,30 @@ export const signUp = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1. Procura o usuário pelo e-mail
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: 'Usuário não encontrado.' });
+
+    // 2. Compara a senha digitada com a senha criptografada
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) return res.status(401).json({ message: 'Senha inválida.' });
+
+    // 3. Se tudo estiver ok, gera um novo Token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: 'Login realizado com sucesso!',
+      data: { token, user },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { signUp } from '../controller/auth.controller.js';
+import { signUp, signIn } from '../controller/auth.controller.js';
+
+
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', signUp); // Aqui nasce o /api/v1/auth/sign-up
+authRouter.post('/sign-in', signIn); // Aqui nasce o /api/v1/auth/sign-in
 
 export default authRouter;
