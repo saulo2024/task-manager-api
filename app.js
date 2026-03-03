733b054b18a7a99801d94a674a8527dc50a1851e
estevesdev <sauloestevesdealmeida27@gmail.com>
2026-03-02 21:52:40 -0400

    fix: corrige imports de cors e sintaxe

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 import express from 'express';
+import cors from 'cors'; // Importe apenas este
 import authRouter from './routes/auth.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import userRouter from './routes/user.routes.js';
 import taskRouter from './routes/task.routes.js';
-import cors from 'cors';
 
 const app = express();
 
-app.use(cors()); // Habilita CORS
-app.use(express.json()); // ESSENCIAL para ler o JSON que você envia
+// CONFIGURAÇÃO DO CORS (O segredo para a Vercel funcionar)
+app.use(cors()); 
 
+app.use(express.json()); // Essencial para ler o JSON
+
+// Conecta ao banco de dados
 connectToDatabase();
 
-// Configura o prefixo da rota
+// Configura as rotas
 app.use('/api/v1/auth', authRouter);
-
 app.use('/api/v1/users', userRouter);
-
 app.use('/api/v1/tasks', taskRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Use a porta da Koyeb ou 3000 local
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta http://localhost:${PORT}`);
+  console.log(`Servidor rodando com sucesso!`);
 });
 
 export default app;
